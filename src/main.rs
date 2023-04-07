1bc6847d87eef57bc0f0a446877a8f8418d3fe10
Sam James <samjojames@gmail.com>
2023-04-07 14:27:51 +0100

    First commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use rand::Rng;
+use std::io;
+fn main() {
+    println!("Welcome to guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("Please provide an input");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
+}
